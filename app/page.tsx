411df7c9a6c597031fda5f3a9c23f936bf594023
Flame1800 <r.shaficoff@gmail.com>
2024-03-14 01:26:24 +0500

    правки статуса  и реализации

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,28 @@
 'use client'
 
 import { Button } from "@/components/ui/button"
-import React, { useEffect, useState } from "react";
-import { ArrowLeftIcon, ArrowRightIcon, Cross1Icon } from '@radix-ui/react-icons'
+import React, { useCallback, useEffect, useState } from "react";
+import { ArrowLeftIcon, Cross1Icon } from '@radix-ui/react-icons'
 
 const nums = ['1', '2', '3', '4', '5', '6', '7', '8', '9', '0'];
 const limit = 4;
 
 type StatusType = 'accepted' | 'rejected' | 'idle';
+
+const statusTheme = {
+  accepted: {
+    title: "Приятного аппетита",
+    color: "green-600"
+  },
+  rejected: {
+    title: "Доступ запрещен",
+    color: "red-500"
+  },
+  idle: {
+    title: "Введите пин-код",
+    color: "white"
+  },
+}
 
 export default function Home() {
   const [code, setCode] = useState<string>('');
@@ -19,11 +34,31 @@ export default function Home() {
     setTimeout(() => {
       setStatus('idle');
       setCode('');
-    }, 3000);
+    }, 1000);
   }, [status])
+  
+  const sendCode = useCallback(async () => {
+    try {
+      const response = await fetch(`https://89.232.236.182:5010/checkaccess/checkbenefits?code=${code}`);
+      const result = await response.json();
+
+      console.log(result);
+    } catch (error) {
+      console.log(error);
+    }
+    if (code === '0000') {
+      return setStatus('rejected');
+    }
+    setStatus('accepted');
+  }, [code]);
+
+  useEffect(() => {
+    if (code.length === limit) sendCode();
+  }, [code, sendCode])
 
   const setCodeHandler = (num: string) => {
     if (code.length === limit) return;
+
     setCode(prev => prev + num);
   }
 
@@ -31,19 +66,15 @@ export default function Home() {
     setCode(code.substring(-1, code.length - 1));
   }
 
-  const sendCodeHandler = () => {
-    if (code === '0000') {
-      return setStatus('rejected');
-    }
-    setStatus('accepted');
-  }
+
+
 
   const renderForm = () => (
     <>
-      <h1 className="scroll-m-25 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-6">
-        Введите пин код
+      <h1 className={`${status !== 'idle' && 'text-white'} scroll-m-25 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-6`}>
+        {statusTheme[status].title}
       </h1>
-      <div className="border-2 rounded-xl p-4 w-full max-w-72 text-center">
+      <div className="bg-white border-2 rounded-xl p-4 w-full max-w-72 text-center">
         <span className={`scroll-m-25 text-4xl font-extrabold tracking-tight lg:text-5xl ${!code.length && 'text-gray-400'}`}>
           {code.length ? code : '0000'}
         </span> 
@@ -81,34 +112,15 @@ export default function Home() {
           return <div key={num}>{renderContent()}</div>;
         })}
       </div>
-      {code.length === 4 && (
-        <Button onClick={sendCodeHandler} className="mt-6 text-xl py-8 px-12 bg-green-600 hover:bg-green-500">
-          Продолжить
-          <ArrowRightIcon className="ml-4" width={25} height={25} />
-        </Button>
-      )}
     </>
   )
 
-  const rederAcceptInfo = () => (
-    <h1 className="scroll-m-25 text-green-600 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-6">
-      Доступ разрешен
-    </h1>
-  )
-
-  const rederRejectInfo = () => (
-    <h1 className="scroll-m-25 text-red-600 pb-2 text-3xl font-semibold tracking-tight first:mt-0 mb-6">
-      Доступ запрещен
-    </h1>
-  )
 
   
   return (
-    <main className="flex min-h-screen flex-col items-center lg:p-24">
+    <main className={`bg-${statusTheme[status].color} flex min-h-screen flex-col items-center lg:p-24`}>
       <section className="max-w-xl w-full flex flex-col items-center mt-10">
-        {status === 'idle' && renderForm()}
-        {status === 'accepted' && rederAcceptInfo()}
-        {status === 'rejected' && rederRejectInfo()}
+        {renderForm()}
       </section>
     </main>
   );
